ece741f8bd9028e2e94a461e7a5e9137fdc009f5
Dean Moses <moses@tacocat.com>
2023-12-12 00:22:04 -0800

    Fetch an entire year's worth of albums

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,35 @@
 
 import { upsertItems } from './lib/db.js';
 import { getAlbumFromZenphoto } from './lib/fetchFromZp.js';
+import { isValidAlbumPath, isValidYearAlbumPath } from './lib/galleryPathUtils.js';
 import { convertAlbum } from './lib/toAwsFromZp.js';
+import { ZenphotoAlbum } from './lib/zenphotoTypes.js';
 
-const albumPaths: string[] = [
-    '/2001/12-31/',
-    '/2001/12-23/',
-    '/2001/12-16/',
-    '/2001/12-14/',
-    '/2001/12-13/',
-    '/2001/12-09/',
-];
-let itemCount = 0;
-for (const albumPath of albumPaths) {
-    console.log(`Fetching ${albumPath}`);
-    const zpAlbum = await getAlbumFromZenphoto(albumPath);
-    const awsItems = convertAlbum(zpAlbum);
-    await upsertItems(awsItems);
-    console.log(`Upserted ${albumPath} - ${awsItems.length} items`);
-    itemCount += awsItems.length;
+const albumPaths: string[] = ['/2001/'];
+
+await processAlbums(albumPaths);
+
+async function processAlbums(albumPaths: string[] | undefined) {
+    if (!albumPaths) return;
+    for (const albumPath of albumPaths) {
+        console.log(`Fetching ${albumPath}`);
+        if (!isValidAlbumPath(albumPath)) throw new Error(`Invalid album path: ${albumPath}`);
+        const zpAlbum = await getAlbumFromZenphoto(albumPath);
+        const awsItems = convertAlbum(zpAlbum);
+        await upsertItems(awsItems);
+        if (isValidYearAlbumPath(albumPath)) {
+            const dayAlbumPaths = getDayAlbumPaths(zpAlbum);
+            if (!dayAlbumPaths) throw new Error(`Year album ${albumPath} has no day albums`);
+            processAlbums(dayAlbumPaths);
+        }
+        console.log(`Upserted ${albumPath} - ${awsItems.length} items`);
+    }
 }
-console.log(`Upserted ${albumPaths.length} albums, ${itemCount} items`);
+
+function getDayAlbumPaths(zpAlbum: ZenphotoAlbum): string[] | undefined {
+    if (!zpAlbum.albums) return;
+    return zpAlbum.albums.map((album) => {
+        if (!album.path) throw new Error(`Album ${album.title} has no path`);
+        return `/${album.path}/`;
+    });
+}
--- src/lib/fetchFromZp.ts
+++ src/lib/fetchFromZp.ts
@@ -1,13 +1,16 @@
-import { isValidAlbumPath } from './galleryPathUtils.js';
+import { isValidAlbumPath, isValidYearAlbumPath } from './galleryPathUtils.js';
 import { ZenphotoAlbum } from './zenphotoTypes.js';
 
 export async function getAlbumFromZenphoto(albumPath: string): Promise<ZenphotoAlbum> {
-    const albumResponse = await fetch(albumPathToZenphotoJsonUrl(albumPath));
+    const albumResponse = await fetch(albumUrl(albumPath));
     const json = await albumResponse.json();
     return json.album;
 }
 
-export function albumPathToZenphotoJsonUrl(albumPath: string): string {
+/** URL of the Zenphoto JSON REST API from which to retrieve the album */
+function albumUrl(albumPath: string): string {
     if (!isValidAlbumPath(albumPath)) throw new Error(`Invalid album path: ${albumPath}`);
-    return `https://tacocat.com/zenphoto${albumPath}?json&pagination=off&depth=1`;
+    if ('/' === albumPath) return 'https://tacocat.com/p_json/root.json';
+    if (isValidYearAlbumPath(albumPath)) return `https://tacocat.com/p_json${albumPath.slice(0, -1)}.json`;
+    return `https://tacocat.com/zenphoto${albumPath}?json`;
 }
