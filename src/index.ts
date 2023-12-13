392fe5da86d6decbc084299832fb2087a2827a01
Dean Moses <moses@tacocat.com>
2023-12-12 18:33:19 -0800

    Support downloading root album
    ... which is useless because I'm getting network errors on the Zenphoto side, which means I'm doing it year by year.

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@
 
 import { upsertItems } from './lib/db.js';
 import { getAlbumFromZenphoto } from './lib/fetchFromZp.js';
-import { isValidAlbumPath, isValidYearAlbumPath } from './lib/galleryPathUtils.js';
+import { isValidAlbumPath, isValidDayAlbumPath } from './lib/galleryPathUtils.js';
 import { convertAlbum } from './lib/toAwsFromZp.js';
 import { ZenphotoAlbum } from './lib/zenphotoTypes.js';
 
-const albumPaths: string[] = ['/2001/'];
+const albumPaths: string[] = ['/2017/']; //, '/2016/', '/2015/', '/2014/'];
+//['/2019/'];
+//['/2023/', '/2022/', '/2021/', '/2020/'];
+//['/2001/', '/2018/'];
 
 await processAlbums(albumPaths);
 
@@ -16,16 +19,20 @@ async function processAlbums(albumPaths: string[] | undefined) {
     if (!albumPaths) return;
     for (const albumPath of albumPaths) {
         console.log(`Fetching ${albumPath}`);
-        if (!isValidAlbumPath(albumPath)) throw new Error(`Invalid album path: ${albumPath}`);
+        if (!isValidAlbumPath(albumPath)) throw new Error(`Invalid album path: [${albumPath}]`);
         const zpAlbum = await getAlbumFromZenphoto(albumPath);
-        const awsItems = convertAlbum(zpAlbum);
-        await upsertItems(awsItems);
-        if (isValidYearAlbumPath(albumPath)) {
-            const dayAlbumPaths = getDayAlbumPaths(zpAlbum);
-            if (!dayAlbumPaths) throw new Error(`Year album ${albumPath} has no day albums`);
-            processAlbums(dayAlbumPaths);
+        // Don't upsert the root album, it doesn't go in the database
+        if ('/' !== albumPath) {
+            const awsItems = convertAlbum(zpAlbum);
+            await upsertItems(awsItems);
+            console.log(`Upserted ${albumPath} - ${awsItems.length} items`);
         }
-        console.log(`Upserted ${albumPath} - ${awsItems.length} items`);
+        // For root and year albums, get child albums
+        if (!isValidDayAlbumPath(albumPath)) {
+            const childAlbumPaths = getDayAlbumPaths(zpAlbum);
+            if (!childAlbumPaths) throw new Error(`Album ${albumPath} has no child albums`);
+            processAlbums(childAlbumPaths);
+        }
     }
 }
 
