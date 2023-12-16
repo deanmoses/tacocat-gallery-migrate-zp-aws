72f4afd52a685fa41afeed19279edfea9bb93121
Dean Moses <moses@tacocat.com>
2023-12-16 14:28:12 -0800

    Added all the versionIds.  I think this is job done for this project.

--- src/addVersionIds.ts
+++ src/addVersionIds.ts
@@ -5,16 +5,38 @@
 
 import { getPaths, setVersionId } from './lib/db.js';
 import { isValidImagePath } from './lib/galleryPathUtils.js';
+import { logMajor, logMinor } from './lib/printUtils.js';
 import { getImageVersionId } from './lib/s3Utils.js';
 
-const startingPath = '/2001/12-31/';
+//const startingPath = '/1943/'; // DONE
+//const startingPath = '/1962/'; // DONE
+//const startingPath = '/1968/'; // DONE
+//const startingPath = '/1969/'; // DONE
+//const startingPath = '/1973/'; // DONE
+//const startingPath = '/1974/'; // DONE
+//const startingPath = '/1975/'; // DONE
+//const startingPath = '/1976/'; // DONE
+//const startingPath = '/1977/'; // DONE
+//const startingPath = '/1978/'; // DONE
+//const startingPath = '/1979/'; // DONE
+//const startingPath = '/198'; // DONE
+//const startingPath = '/199'; // DONE
+//const startingPath = '/200'; // DONE had to lower-case /2001/09-21/lucie_preggie.JPG and lucie_preggie2.JPG
+//const startingPath = '/201'; // DONE
+const startingPath = '/202'; // DONE
+//const startingPath = '/2020/';
+//const startingPath = '/2021/';
+//const startingPath = '/2022/';
+//const startingPath = '/2023/';
+
 const paths = await getPaths(startingPath);
 for (const path of paths) {
     if (!isValidImagePath(path)) {
-        console.log(`Skipping not an image [${path}]`);
+        console.log(`Skipping not an image: ${path}`);
         continue;
     }
     const versionId = await getImageVersionId(path);
-    console.log(`Image [${path}] version ID [${versionId}]`);
+    logMinor(`[${versionId}] ${path}`);
     await setVersionId(path, versionId);
 }
+logMajor(`Added version IDs for ${paths.length} items`);
--- src/createDDBfile.ts
+++ src/createDDBfile.ts
@@ -7,13 +7,19 @@ import { createWriteStream } from 'fs';
 import { getAllItems } from './lib/db.js';
 import { convertToDDBitem } from './lib/toDDB.js';
 import jsonlines from 'jsonlines';
+import { AwsImageItem } from './lib/awsTypes.js';
+import { logMajor } from './lib/printUtils.js';
 
 const writableStream = createWriteStream('ddb/ddb.jsonl');
 const stringifier = jsonlines.stringify();
 stringifier.pipe(writableStream);
 const items = await getAllItems();
 for (const item of items) {
+    if ('image' === item.itemType && !(item as AwsImageItem).versionId) {
+        throw new Error(`Missing versionId for ${item.parentPath}${item.itemName}`);
+    }
     const ddbItem = convertToDDBitem(item);
     stringifier.write({ Item: ddbItem });
 }
 stringifier.end();
+logMajor(`Output ${items.length} items to ddb/ddb.jsonl`);
